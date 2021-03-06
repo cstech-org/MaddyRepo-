e3bd90b46e7c7c9edd9cda5dbc3ff43459822544
coldblooded23 <65030068+coldblooded23@users.noreply.github.com>
2021-03-06 14:59:08 +0530

    remote push

--- gitp/app.py
+++ gitp/app.py
@@ -7,3 +7,8 @@ this is random content adding to check the git repo
 Hello HYD 
 
 HOW ARE YOU 
+
+
+Chnages are made from the remote repository 
+
+same file then the option stash can be used to avoide merge 
